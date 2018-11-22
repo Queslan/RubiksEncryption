d80c528c4df665eb130d575eeb1d5b4103c7fd6e
Queslan <sobota96@wp.pl>
2018-11-22 02:09:31 +0100

    Time measurements

--- main.py
+++ main.py
@@ -1,20 +1,34 @@
+import time
 import cv2
 from FullCube import FullCube as fC
 import ImageProcessing as iP
 
+def measureTIme(file_path):
+    start_time = time.time()
+    image = iP.get_image(file_path)
+    full_cube = fC(iP.make_cubes(image))
 
-image = iP.get_image('bpg_part.png')
-full_cube = fC(iP.make_cubes(image))
+    #moves = ['U', 'L', 'D', 'R', 'F', 'B', 'B', 'B', 'U', 'U', 'U', 'L', 'L', 'L']
+    #moves = ['L', 'R', 'F', 'R', 'B', 'B']
+    moves = ['L', 'D', 'D', 'B', 'L', 'R', 'U', 'U', 'LI', 'DI', 'U', 'R', 'F', 'F']
+    full_cube.move_cube(moves)
 
-#moves = ['U', 'L', 'D', 'R', 'F', 'B', 'B', 'B', 'U', 'U', 'U', 'L', 'L', 'L']
-moves = ['L', 'R', 'F', 'R', 'B', 'B']
-#moves = ['L', 'D', 'D', 'B', 'L', 'R', 'U', 'U', 'LI', 'DI', 'U', 'R', 'F', 'F']
-full_cube.move_cube(moves)
+    #full_cube.solve_cube()
+    #newArray = iP.make_cubic_cut(full_cube.elements)
+    elapsed_time = time.time() - start_time
+    newArray = iP.reassembly_image(full_cube.elements)
+    cv2.namedWindow('myCube', cv2.WINDOW_NORMAL)
+    iP.show_image("myCube", newArray)
+    print(file_path)
+    print(elapsed_time)
 
-full_cube.solve_cube()
-#newArray = iP.make_cubic_cut(full_cube.elements)
-newArray = iP.reassembly_image(full_cube.elements)
-cv2.namedWindow('myCube', cv2.WINDOW_NORMAL)
-iP.show_image("myCube", newArray)
+##### Linear files don't work without solving, problem with reassembly_image function
+#measureTIme('gray8bit.pgm')
+#measureTIme('gray16bit.pgm')
+#measureTIme('gray16bitL.pgm')
+#measureTIme('rgb8bit.ppm')
+#measureTIme('rgb16bit.ppm')
+#measureTIme('rgb16bitL.ppm')
 
+measureTIme("cubesNumbered.png")
 cv2.waitKey(0)
